6b986dce1f35d68d4313ed28b6fb9dd96d768655
1000blxdesss <147636287+1000blxdesss@users.noreply.github.com>
2025-04-23 11:37:03 +0300

    Add files via upload

---
+++ example lua/нити.lua
@@ -0,0 +1,15 @@
+--нити
+local objM = findClass("MenuPck.ListTypes.OtherMenuList")
+local obj = findClass("Base.GameMapObjectsController")
+for j=0,500 do
+sleep(250)
+
+invoke(obj,"actionMapObjectMapPortalUse",1)
+sleep(150)
+for i=1,8 do 
+    if i==5 then
+    invoke(objM,"executeDelegated",3)
+end
+invoke(obj,"actionMapObjectDirectPortal",i,true) end
+invoke(obj,"actionMapObjectMapPortalUse",0)
+end
